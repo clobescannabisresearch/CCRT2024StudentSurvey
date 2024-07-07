e939df820419f0c4f37812da3b34b3cb9a085f7b
clobescannabisresearch <clobescannabisresearch@gmail.com>
2024-07-07 15:53:59 -0700

    Create studentID.js

---
+++ studentID.js
@@ -0,0 +1,64 @@
+define(['questAPI'], function(Quest){
+    var API = new Quest();
+    var isTouch = API.getGlobal().$isTouch;
+	
+    /**
+	* Page prototype
+	*/
+
+    API.addCurrent({
+		baseURL: './images/',
+    });
+
+    API.addPagesSet('basicPage',{
+        noSubmit:false, //Change to true if you don't want to show the submit button.
+        header: 'StudentID', 
+        autoFocus:true, 
+        progressBar:  'Page <%= pagesMeta.number %> out of 1'
+    });
+	
+    /**
+	* Question prototypes
+	*/
+    API.addQuestionsSet('basicQ',{
+        decline: 'true',
+        required : true, 		
+        errorMsg: {
+            required: isTouch 
+                ? 'Please select an answer'
+                : 'Please select an answer'
+        },
+        autoSubmit:'true',
+        numericValues:'true',
+    });
+
+    API.addQuestionsSet('text',{
+        inherit: 'basicQ',
+        type: 'text',
+        noSubmit:false
+    });
+
+	
+    /**
+	*Specific questions
+	*/	
+    API.addQuestionsSet('studentID',{
+        inherit : 'text',
+        name: 'studentID',
+        stem: 'Please provide your student ID#'
+    });
+
+    API.addSequence([
+        {
+            mixer : 'wrapper', 
+            data : [
+                        {
+                        inherit:'basicPage', 
+                        questions : {inherit:'studentID'}           
+                        } 
+                    ]
+                },
+    ]);
+
+    return API.script;
+});
