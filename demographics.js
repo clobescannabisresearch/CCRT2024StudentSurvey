c32e622dd8f1d9385cc7925731cc9c27bd8d3218
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-08-10 14:33:07 -0700

    Update demographics.js

--- demographics.js
+++ demographics.js
@@ -13,7 +13,7 @@ define(['questAPI','underscore'], function(Quest,_){
 	*/
     API.addPagesSet('basicPage',{
         noSubmit:false, //Change to true if you don't want to show the submit button.
-        decline: true,
+        decline: false,
         declineText: isTouch ? 'Decline' : 'Decline to Answer', 
         autoFocus:true, 
         header: 'Demographics',
@@ -25,12 +25,12 @@ define(['questAPI','underscore'], function(Quest,_){
 	* Question prototypes
 	*/
     API.addQuestionsSet('basicQ',{
-        decline: true,
+        decline: false,
         required : true,
         errorMsg: {
             required: isTouch 
-                ? 'Please select an answer, or click \'Decline\'' 
-                : 'Please select an answer, or click \'Decline to Answer\''
+                ? 'Please select an answer' 
+                : 'Please select an answer'
         },
         autoSubmit:true,
         numericValues:true
@@ -85,7 +85,92 @@ define(['questAPI','underscore'], function(Quest,_){
         name:'age',
         stem:'What is your current age?',
         numericValues:true,
-        answers: [18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
+        answers: [
+            {text:'18', value:18},
+            {text:'19',value: 19},
+            {text:'20', value:20},
+            {text:'21', value:21},
+            {text:'22', value:22},
+            {text:'23', value:23},
+            {text:'24', value:24},
+            {text:'25', value:25},
+            {text:'26', value:26},
+            {text:'27', value:27},
+            {text:'28', value:28},
+            {text:'29', value:29},
+            {text:'30', value:30},
+            {text:'31', value:31},
+            {text:'32', value:32},
+            {text:'33', value:33},
+            {text:'34', value:34},
+            {text:'35', value:35},
+            {text:'36', value:36},
+            {text:'37', value:37},
+            {text:'38', value:38},
+            {text:'39', value:39},
+            {text:'40', value:40},
+            {text:'41', value:41},
+            {text:'42', value:42},
+            {text:'43', value:43},
+            {text:'44', value:44},
+            {text:'45', value:45},
+            {text:'46', value:46},
+            {text:'47', value:47},
+            {text:'48', value:48},
+            {text:'49', value:49},
+            {text:'50', value:50},
+            {text:'51', value:51},
+            {text:'52', value:52},
+            {text:'53', value:53},
+            {text:'54', value:54},
+            {text:'55', value:55},
+            {text:'56', value:56},
+            {text:'57', value:57},
+            {text:'58', value:58},
+            {text:'59', value:59},
+            {text:'60', value:60},
+            {text:'61', value:61},
+            {text:'62', value:62},
+            {text:'63', value:63},
+            {text:'64', value:64},
+            {text:'65', value:65},
+            {text:'66', value:66},
+            {text:'67', value:67},
+            {text:'68', value:68},
+            {text:'69', value:69},
+            {text:'70', value:70},
+            {text:'71', value:71},
+            {text:'72', value:72},
+            {text:'73', value:73},
+            {text:'74', value:74},
+            {text:'75', value:75},
+            {text:'76', value:76},
+            {text:'77', value:77},
+            {text:'78', value:78},
+            {text:'79', value:79},
+            {text:'80', value:80},
+            {text:'81', value:81},
+            {text:'82', value:82},
+            {text:'83', value:83},
+            {text:'84', value:84},
+            {text:'85', value:85},
+            {text:'86', value:86},
+            {text:'87', value:87},
+            {text:'88', value:88},
+            {text:'89', value:89},
+            {text:'90', value:90},
+            {text:'91', value:91},
+            {text:'92', value:92},
+            {text:'93', value:93},
+            {text:'94', value:94},
+            {text:'95', value:95},
+            {text:'96', value:96},
+            {text:'97', value:97},
+            {text:'98', value:98},
+            {text:'99', value:99},
+            {text:'100+', value:100},
+
+        ]
     });
 
     API.addQuestionsSet('genderIdentity',{
