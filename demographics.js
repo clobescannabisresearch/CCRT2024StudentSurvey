c6c4f42d0c65b49e7890120b3db932c2a2d507e8
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-08-28 23:19:06 -0700

    Update demographics.js

--- demographics.js
+++ demographics.js
@@ -39,8 +39,7 @@ define(['questAPI','underscore'], function(Quest,_){
     API.addQuestionsSet('singleChoice',{
         inherit: 'basicQ',
         type: 'selectOne', 
-        help: '<%= pagesMeta.number < 10 %>',
-        helpText: 'Tip: For quick response, click to select your answer, and then click again to submit.'
+        autoSubmit:false,
     });
 
     API.addQuestionsSet('singleChoice1',{
